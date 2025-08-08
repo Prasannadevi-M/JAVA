0101c6cf93b4fa34bd259cfcc9fb736c2d8996fe
Prasannadevi-M <2007.prasannadevi@gmail.com>
2025-08-08 20:50:13 +0530

    Create EX2.java

---
+++ EX2.java
@@ -0,0 +1,29 @@
+package oop;
+import java.util.Scanner;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter a numbers:");
+		int num=sc.nextInt();
+		boolean isPrime=true;
+		if (num<=1) {
+			isPrime=false;
+			
+		}else {
+			for(int i=2;i<=Math.sqrt(num);i++) {
+				if (num%i==0) {
+					isPrime=false;
+					break;
+				}
+			}
+		}
+		if (isPrime) {
+			System.out.println(num+" is a prime number.");
+		}else {
+			System.out.println(num+ " is not a prime number.");
+		}
+		sc.close();
+	}
